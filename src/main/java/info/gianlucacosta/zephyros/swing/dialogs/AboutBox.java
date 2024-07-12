cf49aeea19ae5630d5edc3c063a4f46717158676
Gianluca Costa <gianluca@gianlucacosta.info>
2024-07-12 11:02:56 +0200

    Revise code

--- src/main/java/info/gianlucacosta/zephyros/swing/dialogs/AboutBox.java
+++ src/main/java/info/gianlucacosta/zephyros/swing/dialogs/AboutBox.java
@@ -1,7 +1,5 @@
 package info.gianlucacosta.zephyros.swing.dialogs;
 
-import info.gianlucacosta.zephyros.os.User;
-
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -17,178 +15,147 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
-import java.util.Optional;
 
 /**
  * Standard, handy "About..." dialog
  */
 public class AboutBox extends JDialog {
-    public AboutBox(
-            Image windowIcon,
-            Image productImage,
-            String productName,
-            String productVersion,
-            String copyrightHolder
-    ) {
-        setIconImage(windowIcon);
+        public AboutBox(
+                        Image windowIcon,
+                        Image productImage,
+                        String productName,
+                        String productVersion,
+                        String copyrightHolder) {
+                setIconImage(windowIcon);
 
-        setTitle("About...");
+                setTitle("About...");
 
-        setResizable(false);
+                setResizable(false);
 
-        setModal(true);
+                setModal(true);
 
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-        setMinimumSize(
-                new Dimension(500, 0)
-        );
+                setMinimumSize(
+                                new Dimension(500, 0));
 
+                setLayout(new BorderLayout());
 
-        setLayout(new BorderLayout());
+                add(new JPanel() {
+                        {
+                                setBackground(Color.WHITE);
 
-        add(new JPanel() {
-                {
-                    setBackground(Color.WHITE);
+                                setBorder(
+                                                BorderFactory.createEmptyBorder(
+                                                                20, 20, 20, 20));
 
-                    setBorder(
-                            BorderFactory.createEmptyBorder(
-                                    20, 20, 20, 20
-                            )
-                    );
+                                setLayout(
+                                                new BoxLayout(
+                                                                this,
+                                                                BoxLayout.PAGE_AXIS));
 
-                    setLayout(
-                            new BoxLayout(
-                                    this,
-                                    BoxLayout.PAGE_AXIS
-                            )
-                    );
+                                add(
+                                                new JLabel(
+                                                                new ImageIcon(productImage)) {
+                                                        {
+                                                                setBorder(
+                                                                                BorderFactory.createEmptyBorder(
+                                                                                                0,
+                                                                                                0,
+                                                                                                20,
+                                                                                                0));
 
-                    add(
-                            new JLabel(
-                                    new ImageIcon(productImage)
-                            ) {
-                                {
-                                    setBorder(
-                                            BorderFactory.createEmptyBorder(
-                                                    0,
-                                                    0,
-                                                    20,
-                                                    0
-                                            )
-                                    );
+                                                                setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        }
+                                                });
 
-                                    setAlignmentX(Component.CENTER_ALIGNMENT);
-                                }
-                            }
-                    );
+                                add(
+                                                new JLabel(productName) {
+                                                        {
+                                                                setFont(
+                                                                                getFont()
+                                                                                                .deriveFont(Font.BOLD,
+                                                                                                                45));
 
+                                                                setBorder(
+                                                                                BorderFactory.createEmptyBorder(
+                                                                                                0,
+                                                                                                0,
+                                                                                                15,
+                                                                                                0));
 
-                    add(
-                            new JLabel(productName) {
-                                {
-                                    setFont(
-                                            getFont()
-                                                    .deriveFont(Font.BOLD, 45)
-                                    );
+                                                                setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        }
+                                                });
 
-                                    setBorder(
-                                            BorderFactory.createEmptyBorder(
-                                                    0,
-                                                    0,
-                                                    15,
-                                                    0
-                                            )
-                                    );
+                                add(
+                                                new JLabel(
+                                                                String.format(
+                                                                                "Version %s",
+                                                                                productVersion)) {
+                                                        {
+                                                                setFont(
+                                                                                getFont()
+                                                                                                .deriveFont(Font.BOLD,
+                                                                                                                20));
 
-                                    setAlignmentX(Component.CENTER_ALIGNMENT);
-                                }
-                            }
-                    );
+                                                                setBorder(
+                                                                                BorderFactory.createEmptyBorder(
+                                                                                                0,
+                                                                                                0,
+                                                                                                25,
+                                                                                                0));
 
+                                                                setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        }
+                                                });
 
-                    add(
-                            new JLabel(
-                                    String.format(
-                                            "Version %s",
-                                            productVersion
-                                    )
-                            ) {
-                                {
-                                    setFont(
-                                            getFont()
-                                                    .deriveFont(Font.BOLD, 20)
-                                    );
+                                add(
+                                                new JLabel(
+                                                                String.format(
+                                                                                "Copyright © %s",
+                                                                                copyrightHolder)) {
+                                                        {
+                                                                setFont(
+                                                                                getFont()
+                                                                                                .deriveFont(Font.ITALIC,
+                                                                                                                13));
 
-                                    setBorder(
-                                            BorderFactory.createEmptyBorder(
-                                                    0,
-                                                    0,
-                                                    25,
-                                                    0
-                                            )
-                                    );
-
-                                    setAlignmentX(Component.CENTER_ALIGNMENT);
-                                }
-                            }
-                    );
-
-                    add(
-                            new JLabel(
-                                    String.format(
-                                            "Copyright © %s",
-                                            copyrightHolder
-                                    )
-                            ) {
-                                {
-                                    setFont(
-                                            getFont()
-                                                    .deriveFont(Font.ITALIC, 13)
-                                    );
-
-                                    setAlignmentX(Component.CENTER_ALIGNMENT);
-                                }
-                            }
-                    );
-                }
-            },
-
-                BorderLayout.CENTER
-        );
-
-
-        add(
-                new JPanel() {
-                    {
-                        setBackground(Color.WHITE);
-
-                        setBorder(
-                                BorderFactory.createEmptyBorder(0, 20, 20, 20)
-                        );
-
-                        setLayout(new FlowLayout());
-
-                        add(
-                                new JButton("OK") {
-                                    {
-                                        addActionListener(event ->
-                                                AboutBox.this.dispose()
-                                        );
-
-                                        AboutBox.this.getRootPane().setDefaultButton(this);
-                                    }
-                                }
-                        );
-                    }
+                                                                setAlignmentX(Component.CENTER_ALIGNMENT);
+                                                        }
+                                                });
+                        }
                 },
 
-                BorderLayout.SOUTH
-        );
+                                BorderLayout.CENTER);
 
+                add(
+                                new JPanel() {
+                                        {
+                                                setBackground(Color.WHITE);
 
-        pack();
+                                                setBorder(
+                                                                BorderFactory.createEmptyBorder(0, 20, 20, 20));
 
-        setLocationRelativeTo(null);
-    }
+                                                setLayout(new FlowLayout());
+
+                                                add(
+                                                                new JButton("OK") {
+                                                                        {
+                                                                                addActionListener(event -> AboutBox.this
+                                                                                                .dispose());
+
+                                                                                AboutBox.this.getRootPane()
+                                                                                                .setDefaultButton(this);
+                                                                        }
+                                                                });
+                                        }
+                                },
+
+                                BorderLayout.SOUTH);
+
+                pack();
+
+                setLocationRelativeTo(null);
+        }
 }
